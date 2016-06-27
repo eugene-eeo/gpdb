2c073a7572f4513f398895913866e487f7ab89ff
Eeo Jun <packwolf58@gmail.com>
2016-06-27 09:14:02 +0800

    Refactor tests

--- test.py
+++ test.py
@@ -12,15 +12,13 @@ Options:
   -M M       ending messages per node   [default: 10].
 """
 
-import json
+import sys
+from concurrent.futures import ProcessPoolExecutor
 from itertools import repeat
 from docopt import docopt
+
+import newlinejson as nlj
 from gpdb import simulate
-from concurrent.futures import ProcessPoolExecutor
-
-
-def emit(D):
-    print(json.dumps(D))
 
 
 def task(arg):
@@ -28,31 +26,32 @@ def task(arg):
     return len(list(simulate(100, bandwidth=B, messages=M)))
 
 
-def gensim(M, brange, executor, times):
-    for b in range(*brange):
-        results = list(executor.map(
-            task,
-            repeat((M, b), times)
-            ))
-        yield b, results
+def run(dst, mrange, brange, executor, times):
+    dst.write(['M', 'B', 'results'])
+    for M in range(*mrange):
+        for B in range(*brange):
+            results = list(executor.map(
+                task,
+                repeat((M, B), times)
+                ))
+            dst.write([M, B, results])
 
 
 def main():
     args = docopt(__doc__, version='0.1')
     m0, m1 = int(args['-m']), int(args['-M']) + 1
     b0, b1 = int(args['-b']), int(args['-B']) + 1
-    T      = int(args['-t'])
+    times  = int(args['-t'])
 
     assert m0 < m1
     assert b0 < b1
 
+    mrange = (m0, m1)
     brange = (b0, b1)
 
-    emit(['M', 'B', 'results'])
-    with ProcessPoolExecutor() as executor:
-        for M in range(m0, m1):
-            for B, results in gensim(M, brange, executor, T):
-                emit([M, B, results])
+    with nlj.open(sys.stdout, 'w') as dst:
+        with ProcessPoolExecutor() as exe:
+            run(dst, mrange, brange, exe, times)
 
 
 if __name__ == '__main__':
