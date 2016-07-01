79e3742dfad7b561d8545cd93f950e13b9306575
Eeo Jun <packwolf58@gmail.com>
2016-07-02 07:48:54 +0800

    More styling options

--- postproc/table.py
+++ postproc/table.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
+"""
+Usage:
+  table.py [--fmt=<format>]
+  table.py (--help | -h)
 
+Options:
+  --fmt=<format>   Table format to use. [default:rst]
+  --help -h        Shows the current screen.
+"""
+
+import docopt
 from tabulate import tabulate
 import newlinejson as nlj
 import sys
 
 
 def main():
-    stdin  = sys.stdin
+    args = docopt.docopt(__doc__)
+    tfmt = args['--fmt']
+    stdin = sys.stdin
 
     with nlj.open(stdin) as src:
         cols = next(src)
         data = [line for line in src]
-        print(tabulate(data, tablefmt='rst', headers=cols))
+        print(tabulate(data, tablefmt=tfmt, headers=cols))
 
 
 if __name__ == '__main__':
